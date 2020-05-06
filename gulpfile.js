4c0f1d3ee88f05c366e9553c2bafa1502979e036
Boris <boris021@mail.ru>
2020-05-06 12:03:10 +0500

    feat: add file grid, interface, normalize, variables, header styles in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-const { src, dest, watch } = require("gulp");
+const {
+  src,
+  dest,
+  watch
+} = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
 
@@ -7,19 +11,19 @@ function bs() {
   serveSass();
   browserSync.init({
     server: {
-      baseDir: "./",
-    },
+      baseDir: "./"
+    }
   });
   watch("./*.html").on("change", browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on("change", browserSync.reload);
-}
+};
 
 function serveSass() {
   return src("./sass/*.sass")
     .pipe(sass())
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-}
+};
 
 exports.serve = bs;
